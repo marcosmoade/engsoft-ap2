f4d8d180468dd0d124d8516502de5947592a5bb7
Marcos Vinícius <marcosmoade@hotmail.com>
2021-03-05 02:39:01 -0300

    Commit 18

--- Statement.java
+++ Statement.java
@@ -1,3 +1,17 @@
-public class Statement {
+import java.util.Enumeration;
 
+public class Statement {
+    public String value(Customer aCustomer) {
+        Enumeration rentals = aCustomer.getRentals();
+        String result = aCustomer.getName() + "\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // show figures for each rental
+            result += " " + each.getMovie().getTitle() + " " + String.valueOf(each.getCharge()) + "\n";
+        }
+        // add footer lines
+        result += String.valueOf(aCustomer.getTotalCharge()) + "\n";
+        result += String.valueOf(aCustomer.getTotalFrequentRenterPoints());
+        return result;
+    }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,19 +1,5 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = aCustomer.getName() + "\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            // show figures for each rental
-            result += " " + each.getMovie().getTitle() + " " + String.valueOf(each.getCharge()) + "\n";
-        }
-        // add footer lines
-        result += String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += String.valueOf(aCustomer.getTotalFrequentRenterPoints());
-        return result;
-    }
-
+    
 }
